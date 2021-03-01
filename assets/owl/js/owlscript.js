69a687765c997218b08a82324900d1d02abbac8d
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-03-01 18:17:25 +0300

    homepage-clients and homepage-find-job responsive on sm

---
+++ assets/owl/js/owlscript.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:false
+      },
+      600:{
+          items:1,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:false,
+          loop:false
+      }
+  }
+})
