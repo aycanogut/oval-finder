d394e0b2575b660c444ceb8daa4011b308e1ecd7
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-03-03 17:22:48 +0300

    testimonials-carousel controls updated on lg>

---
+++ .history/assets/owl/js/owlscript_20210303150546.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:false,
+      },
+      600:{
+          items:2,
+          nav:false
+      },
+      992:{
+          items:1,
+          nav:false,
+          loop:false,
+      }
+  }
+})
