cabca32d53b2fdf19612e1e4554ee4d9df6c24a6
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-04-24 16:17:40 +0300

    gitignore updated

---
+++ assets/js/nav.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger-toggler");
+  const nav = document.querySelector(".navbar-collapse");
+  const navLinks = document.querySelectorAll('.navbar-collapse li')
+
+  burger.addEventListener('click', () => {
+    // nav toggle
+
+    nav.classList.toggle('nav-active');
+  });
+
+}
+
+navSlide();
